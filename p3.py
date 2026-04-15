186c5f547e4a909f3f46f20ba9198fde4ec249c4
Chandra-CST <thakur.shubh2004@gmail.com>
2026-04-15 22:08:55 +0530

    Create p3.py
    While loop basics

---
+++ p3.py
@@ -0,0 +1,15 @@
+# WHILE LOOP: Used until a condition is met unlike for loop.
+
+# PRINT NUMBERS FROM 10 TO 1 USING WHILE LOOP:
+
+ i = 10
+ while i >= 1:
+     print(i)
+   i -= 1
+
+# PEINT EVEN NUMBERS TILL n:
+
+n = int(input('Enter a number:'))
+while n % 2 == 0:
+    print(n)
+    n = n+1
