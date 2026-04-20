edfdab9ec6cc89e3ec56265c68de3e62228f17c7
Chandra-CST <thakur.shubh2004@gmail.com>
2026-04-20 17:03:47 +0530

    Create Modules.py

---
+++ Modules.py
@@ -0,0 +1,41 @@
+
+def add(a, b):
+    return a + b
+
+def multiplication(x, y):
+   return x*y
+
+def sub(a, b):
+    return a - b
+
+def main():
+    print("Welcome to main function.")
+    print(add(4,6))
+    print(add(65,6))
+    print(multiplication(4, 5))
+    print(sub(233,655))
+    print(sub(214,56))
+
+
+if __name__ == "__main__":
+ main()
+
+# We use these modules for code reusability,
+'''
+Creating a file called main.py and then importing the functions needed to solve or perform
+tasks without the need of writing code again
+
+'''
+
+
+
+# import Main
+# print(Main.add(4,6))
+
+# print(Main.add(2,3))
+
+
+# print(Main.multiplication(2,3))
+
+
+# print(Main.multiplication(768,989))
